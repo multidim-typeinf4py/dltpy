6b2007b92c943cedd88d7cfef6e7c8d9404695c5
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-12-08 04:56:12 +0100

    Use more resources

--- slurm/complete.sh
+++ slurm/complete.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=dltpy-training
-#SBATCH --output=slurm-runs/training.txt
-#SBATCH --gres=gpu:1
+## Logging
+# SBATCH --job-name=dltpy-training
+# SBATCH --output=slurm-runs/training.txt
+# SBATCH --mail-user=benjamin.sparks@stud.uni-heidelberg.de
+
+## Resources
+# SBATCH --nodes=count 8
+# SBATCH --gres=gpu:1
+# SBATCH --time=12:00:00
+
+
+
 
 source .venv/bin/activate
 pip install -r requirements.txt
 
-python preprocessing/pipeline.py
+python preprocessing/pipeline.py --jobs 8
 
 (cd input-preparation; python generate_df.py)
 (cd input-preparation; python df_to_vec.py)
